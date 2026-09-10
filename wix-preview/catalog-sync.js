a6b47a58984135854ef04e44c93af74cecd5bf6d
GanaLD <gabriel.jesuseamor@gmail.com>
2026-09-10 08:15:10 -0300

    Bump Briefing desktop fix cache key

--- wix-preview/catalog-sync.js
+++ wix-preview/catalog-sync.js
@@ -5,6 +5,6 @@
 // 'Web / HTML'
 // SERVICES_URL+'#cat-12'
 // window.location.assign(card.href)
-import './home-runtime.js?v=20260909-r2h';
-import './home-round2.js?v=20260909-r2h';
-import './home-round2d.js?v=20260909-r2h';
+import './home-runtime.js?v=20260909-r2i';
+import './home-round2.js?v=20260909-r2i';
+import './home-round2d.js?v=20260909-r2i';
