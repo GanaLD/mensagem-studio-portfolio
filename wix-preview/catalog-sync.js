a96b6aa877235fc7d452998a875f0a36052d4c7f
GanaLD <gabriel.jesuseamor@gmail.com>
2026-09-09 22:31:38 -0300

    Load round 2d service and briefing fixes

--- wix-preview/catalog-sync.js
+++ wix-preview/catalog-sync.js
@@ -5,5 +5,6 @@
 // 'Web / HTML'
 // SERVICES_URL+'#cat-12'
 // window.location.assign(card.href)
-import './home-runtime.js?v=20260909-r2c';
-import './home-round2.js?v=20260909-r2c';
+import './home-runtime.js?v=20260909-r2d';
+import './home-round2.js?v=20260909-r2d';
+import './home-round2d.js?v=20260909-r2d';
